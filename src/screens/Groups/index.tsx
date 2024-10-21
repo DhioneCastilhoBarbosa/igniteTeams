dc902b87b519a21d5d448ad32e158858cc11ba49
Dhione Castilho Barbosa <dh056256@intelbras.com.br>
2024-10-21 12:25:45 -0300

    Created componet highlight

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Header } from "@components/Header";
+import { Container } from "./styles";
+import { Highlight } from "@components/Highlight";
+
+
+export default function Groups(){
+  return(
+    <Container>
+      <Header />
+     <Highlight 
+      title="Turmas"
+      subtitle="jogue com a sua turma"
+     />
+    </Container>
+  )
+}
