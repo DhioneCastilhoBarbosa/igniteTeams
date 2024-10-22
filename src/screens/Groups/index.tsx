c1d614b685bfde39a35af95852a7362ddc4e51ba
Dhione Castilho Barbosa <dh056256@intelbras.com.br>
2024-10-22 13:27:27 -0300

    feat: create component GroupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,16 +2,18 @@ import React from "react";
 import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { Highlight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
 
 
 export default function Groups(){
   return(
     <Container>
       <Header />
-     <Highlight 
-      title="Turmas"
-      subtitle="jogue com a sua turma"
-     />
+      <Highlight 
+        title="Turmas"
+        subtitle="jogue com a sua turma"
+      />
+      <GroupCard title="Galera do Ignite"/>
     </Container>
   )
 }
